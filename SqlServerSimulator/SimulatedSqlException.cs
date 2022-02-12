c90d53a5522422a052853ac71f0f71a26c130317
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2022-02-12 17:15:02 -0600

    SimulatedSqlException now extends DbException improving the fidelity of the simulation, though it doesn't yet offer any of the added features of that type.

--- SqlServerSimulator/SimulatedSqlException.cs
+++ SqlServerSimulator/SimulatedSqlException.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using System.Data.Common;
 
 namespace SqlServerSimulator;
 
 /// <summary>
 /// Describes a simulated SQL exception.
 /// </summary>
-public sealed class SimulatedSqlException : Exception
+public sealed class SimulatedSqlException : DbException
 {
     internal SimulatedSqlException(string? message)
         : base(message ?? "Simulated exception with no message.")
