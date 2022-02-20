99cfbc4cb652f50c96aca9d10ec7af0c99b79f98
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2022-02-20 15:34:03 -0600

    Added capability for SQL exceptions to capture multiple errors.

---
+++ SqlServerSimulator/SimulatedSqlError.cs
@@ -0,0 +1,20 @@
+﻿namespace SqlServerSimulator;
+
+internal sealed class SimulatedSqlError
+{
+    internal SimulatedSqlError(string message, int number, byte @class, byte state)
+    {
+        this.Message = message;
+        this.Number = number;
+        this.Class = @class;
+        this.State = state;
+    }
+
+    public string Message { get; }
+
+    public int Number { get; }
+
+    public byte Class { get; }
+
+    public byte State { get; }
+}
--- SqlServerSimulator/SimulatedSqlException.cs
+++ SqlServerSimulator/SimulatedSqlException.cs
@@ -8,17 +8,34 @@ namespace SqlServerSimulator;
 internal sealed class SimulatedSqlException : DbException
 {
     internal SimulatedSqlException(string? message)
+        : this(message, Array.Empty<SimulatedSqlError>())
+    {
+    }
+
+    internal SimulatedSqlException(string message, int number, byte @class, byte state)
+        : this(message, new[] { new SimulatedSqlError(message, number, @class, state)})
+    {
+    }
+
+    internal SimulatedSqlException(string? message, IReadOnlyList<SimulatedSqlError>? errors)
         : base(message ?? "Simulated exception with no message.")
     {
         base.HResult = unchecked((int)0x80131904);
-    }
+        
+        errors = this.Errors = errors ?? Array.Empty<SimulatedSqlError>();
 
-    internal SimulatedSqlException(string? message, int number, byte @class, byte state)
-        : this(message)
-    {
-        this.Number = number;
-        this.Class = @class;
-        this.State = state;
+        if (errors.Count == 0)
+        {
+            this.Errors = new [] { new SimulatedSqlError(base.Message, 0, 0, 0)};
+
+            return;
+        }
+
+        var firstError = errors[0];
+
+        this.Number = firstError.Number;
+        this.Class = firstError.Class;
+        this.State = firstError.State;
     }
 
     /// <inheritdoc/>
@@ -32,4 +49,6 @@ internal sealed class SimulatedSqlException : DbException
     public byte Class { get; }
 
     public byte State { get; }
+
+    public IReadOnlyList<SimulatedSqlError> Errors { get; }
 }
