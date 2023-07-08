b790aa9a042b7773faeeeb288ae1357cfadf107d
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2023-07-08 15:18:57 -0500

    Used a better solution for context-associated DbSets.

--- SqlServerSimulator.Tests.EFCore/EFCoreBasics.cs
+++ SqlServerSimulator.Tests.EFCore/EFCoreBasics.cs
@@ -31,9 +31,7 @@ public class EFCoreBasics
         {
         }
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public TestContext(Simulation simulation)
-#pragma warning restore
         {
             this.Simulation = simulation;
         }
@@ -44,9 +42,7 @@ public class EFCoreBasics
             optionsBuilder.UseSqlServer(this.Simulation.CreateDbConnection());
         }
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public DbSet<TestRow> Rows { get; set; }
-#pragma warning restore
+        public DbSet<TestRow> Rows => Set<TestRow>();
     }
 
     [TestMethod]
